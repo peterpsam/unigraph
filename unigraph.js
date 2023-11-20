b23909ab5b1e46300a6c849400b21e3991b29b6e
peterpsam <104079783+peterpsam@users.noreply.github.com>
2023-11-20 00:28:13 -0800

    Add files via upload

---
+++ unigraph.js
@@ -0,0 +1,8 @@
+let forImage = document.getElementById('slide-1');
+
+let newImage = './unigraph 2.jpeg';
+
+function replaceImage(){
+    forImage.innerHTML = newImage
+    console.log(forImage)
+}
